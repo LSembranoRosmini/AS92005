1de51522a4bd833e940fb69d365ec3d7549872e9
LSembranoRosmini <21137@rosmini.school.nz>
2025-03-25 10:50:15 +1300

    revamp entire player node to rigid body 2d

--- Scripts/acorn_player.gd
+++ Scripts/acorn_player.gd
@@ -1,55 +1,21 @@
-extends CharacterBody2D
+extends RigidBody2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2
+@onready var raycast = $RayCast2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-@export var player_index: int = 1
-@onready var animated_sprite = $AnimatedSprite2D
+const MOVE_SPEED = 50
+const MAX_SPEED = 100
+const JUMP_FORCE = -300
 
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-# Handle jump for separate players.
-	#P1 Jump
-	if player_index == 0:
-		if Input.is_action_just_pressed("p1_jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-	#P2 Jump
-	if player_index == 1:
-		if Input.is_action_just_pressed("p2_jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
-# Get the input direction and handle the movement/deceleration.
-	#P1 Controls
-	if player_index == 0:
-		var direction := Input.get_axis("p1_left", "p1_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		if direction > 0:
-			animated_sprite.flip_h = false
-		elif direction < 0:
-			animated_sprite.flip_h = true
-
-
-	#P2 Controls
-	if player_index == 1:
-		var direction := Input.get_axis("p2_left", "p2_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		if direction > 0:
-			animated_sprite.flip_h = true
-		elif direction < 0:
-			animated_sprite.flip_h = false
-
-
-
-
-	move_and_slide()
+func _physics_process(delta):
+	var direction = Input.get_axis("p1_left", "p1_right")
+	var force = Vector2.ZERO
+	
+	if direction:
+		force.x = MOVE_SPEED * direction
+		if abs(linear_velocity) > MAX_SPEED: linear_velocity.x = MAX_SPEED * direction
+	
+	apply_central_force(force)
+	
+	
+	
